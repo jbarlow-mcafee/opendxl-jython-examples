7194e92e8df3f92b215eedffc88e317beffc3c32
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-05-23 16:14:02 -0700

    Add multiple topics for one service to the service provider example

--- service-provider/src/main/java/com/att/cso/opendxl/ServiceProviderMain.java
+++ service-provider/src/main/java/com/att/cso/opendxl/ServiceProviderMain.java
@@ -1,7 +1,11 @@
 package com.att.cso.opendxl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.att.cso.opendxl.jython.client.JythonFactory;
 import com.att.cso.opendxl.jython.client.exceptions.DxlJythonException;
+import com.att.cso.opendxl.jython.client.interfaces.DxlCallbackInterface;
 import com.att.cso.opendxl.jython.client.interfaces.DxlProviderInterface;
 
 public class ServiceProviderMain {
@@ -9,8 +13,11 @@ public class ServiceProviderMain {
 
     public static void main(String[] args) {
 
-        ServiceThread thread = new ServiceThread();
-        thread.start();
+        Service1Thread service1Thread = new Service1Thread();
+        service1Thread.start();
+
+        Service2Thread service2Thread = new Service2Thread();
+        service2Thread.start();
 
         while (!SHUTDOWN_RECEIVED) {
             System.out.println("Waiting for message for service...");
@@ -21,15 +28,24 @@ public class ServiceProviderMain {
         }
         System.out.println("Exited service loop...");
 
-        thread.getInterface().stop();
+        service1Thread.getInterface().stop();
+        try {
+            service1Thread.join();
+        } catch (InterruptedException e) {
+        }
+
+        service2Thread.getInterface().stop();
+        try {
+            service2Thread.join();
+        } catch (InterruptedException e) {
+        }
+
         System.out.println("Issued stop to Python loop");
     }
-
 }
 
-class ServiceThread extends Thread {
+class Service1Thread extends Thread {
     private String configFile = "./dxlclient.config";
-    private String topic = "/my/service/test/dxlJythonTest";
     private DxlProviderInterface dxl = null;
 
     public void run() {
@@ -42,8 +58,11 @@ class ServiceThread extends Thread {
         }
 
         DxlCallbackImplementer dxlCallback = new DxlCallbackImplementer();
+
         try {
-            dxl.start(configFile, "/att/test/service", topic, dxlCallback);
+            dxl.start(configFile, "/att/test/dxl",
+                    "/my/service/test/dxlJythonTest",
+                    dxlCallback);
         } catch (DxlJythonException e) {
             System.out.println("Exception caught starting the provider");
             System.out.println("ErrorCode: " + e.getErrorCode() + "  Error message: '" + e.getMessage() + "'");
@@ -55,5 +74,39 @@ class ServiceThread extends Thread {
     public DxlProviderInterface getInterface() {
         return dxl;
     }
+}
 
+class Service2Thread extends Thread {
+    private String configFile = "./dxlclient.config";
+    private DxlProviderInterface dxl = null;
+
+    public void run() {
+        JythonFactory jf = JythonFactory.getInstance();
+
+        dxl = jf.getDxlProviderInterface();
+        if (dxl == null) {
+            System.out.println("Unable to get the Jython provider object");
+            System.exit(0);
+        }
+
+        DxlCallbackImplementer dxlCallback = new DxlCallbackImplementer();
+
+        try {
+            Map<String, DxlCallbackInterface> callbacksByTopic = new HashMap<>();
+            callbacksByTopic.put("/my/service/test/dxlJythonTest1",
+                    dxlCallback);
+            callbacksByTopic.put("/my/service/test/dxlJythonTest2",
+                    dxlCallback);
+            dxl.start(configFile, "/att/test/service2", callbacksByTopic);
+        } catch (DxlJythonException e) {
+            System.out.println("Exception caught starting the provider");
+            System.out.println("ErrorCode: " + e.getErrorCode() + "  Error message: '" + e.getMessage() + "'");
+            dxl.stop();
+            System.exit(0);
+        }
+    }
+
+    public DxlProviderInterface getInterface() {
+        return dxl;
+    }
 }
